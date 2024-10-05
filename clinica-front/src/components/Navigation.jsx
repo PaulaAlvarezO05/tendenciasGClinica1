586ea56caa872005e7399f68bb79a8636de10cac
Liahbatdev <valentina.meneses033@gmail.com>
2024-10-05 13:21:57 -0500

    Creación de barra de navegación para comunicar a diferentes secciones de la app

--- clinica-front/src/components/Navigation.jsx
+++ clinica-front/src/components/Navigation.jsx
@@ -1,17 +1,45 @@
+import React from 'react';
 import {Link} from 'react-router-dom'
 
-export function Navigation() {
+const Navigation = () => {
     return (
-        <div>
-    <Link to ="/patients">
-    <h1>Patients App</h1>
-    </Link> 
-    <Link to ="/patients-create">create patient</Link> 
-        </div>
-    )
-}
-
-
-<div>
-
-</div>
+      <nav className="flex justify-between py-3">
+        <ul className="flex space-x-4">
+          <li>
+            <Link
+              to="/patients"
+              className="inline-block px-4 py-2 text-white bg-purple-700 rounded hover:bg-purple-800 transition"
+            >
+              Patients
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/appointments"
+              className="inline-block px-4 py-2 text-white bg-purple-700 rounded hover:bg-purple-800 transition"
+            >
+              Appointments
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/patients-create"
+              className="inline-block px-4 py-2 text-white bg-purple-700 rounded hover:bg-purple-800 transition"
+            >
+              Create Patient
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/appointments/new"
+              className="inline-block px-4 py-2 text-white bg-purple-700 rounded hover:bg-purple-800 transition"
+            >
+              New Appointment
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  };
+  
+  export default Navigation;
